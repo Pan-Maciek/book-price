6d789aff33dc547552ccc3d848eb18ffaa003ab7
koziejka <koziejka.com@gmail.com>
2019-03-07 02:40:53 +0100

    added loading animations

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 const readline = require('readline')
 const chalk = require('chalk')
+const { Spinner } = require('cli-spinner')
 
 const encodeUri = x => encodeURIComponent(x.replace(/\s+/g, '+'))
 const avg = arr => Math.floor(arr.reduce((acc, x) => acc + x) / arr.length * 100) / 100
@@ -14,13 +15,17 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
-
 rl.on('line', async data => {
+  const spinner = new Spinner
+  spinner.setSpinnerString(1)
+  spinner.start()
   const [title, author] = data.split(';').map(x => x.trim())
   try {
     const prices = await getInfo(title, author)
+    spinner.stop(true)
     console.log(`min: ${chalk.red(Math.min(...prices))} avg: ${chalk.yellow(avg(prices))} max: ${chalk.green(Math.max(...prices))}`)
   } catch (error) {
+    spinner.stop(true)
     console.log(chalk.red('Wystąpił błąd, możliwe powody: źle wprowadzone dane, brak połączenia internetowego, brak książki w bazie danych'))
   }
 })
